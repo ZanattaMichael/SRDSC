2a136d544d543e5b036bad10de2ef590c1de34b4
Michael Zanatta <mzanatta@outlook.com.au>
2022-07-11 18:42:20 +1000

    Updates: Updated Module Parameters Refactored Code to Suit
    Added:
    Started on Start-SRDSCConfiguration
    - Compiling the MOF Files
    - Renaming Files to GUIDs
    - TODO: Deploying to SR Pull Server

--- ScriptRunnerScripts/Module/Private/Set-ModuleParameters.ps1
+++ ScriptRunnerScripts/Module/Private/Set-ModuleParameters.ps1
@@ -1,5 +1,36 @@
-$Global:SRDatum = [PSCustomObject]@{
-    DatumConfigurationPath = 'D:\Git\DSC-ScriptRunner\DSC-ScriptRunner\'
-    DatumConfigurationFile = 'D:\Git\DSC-ScriptRunner\DSC-ScriptRunner\source\Datum.yml'
-    NodeTemplateFile = 'D:\Git\DSC-ScriptRunner\DSC-ScriptRunner\ScriptRunnerScripts\Template\NodeTemplateConfiguration.yml'
+
+Function Set-ModuleParameters {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [String]
+        $DatumModulePath,
+        [Parameter(Mandatory)]
+        [String]        
+        $ScriptRunnerModulePath
+    )
+
+    $Global:ScriptRunner = [PSCustomObject]@{
+
+        ScriptRunner = [PSCustomObject]@{
+            NodeTemplateFile = '{0}\Template\NodeTemplateConfiguration.yml' -f $ScriptRunnerModulePath                           
+        }
+    
+        DSCPullServer = [PSCustomObject]@{
+            DSCPullServerWebAddress = 'http://SCRIPTRUNNER01:8080'
+            MOFNodeRegistrationFile = '{0}\Configuration\MOFNodeRegistrationFile.clixml' -f $ScriptRunnerModulePath
+        }
+
+        DatumModule = [PSCustomObject]@{
+            ConfigurationPath = $DatumModulePath
+            RenamedMOFOutput = '{0}\output\RenamedMOF' -f $DatumModulePath
+            ConfigurationFile = '{0}\Datum.yml' -f $DatumModulePath
+            CompiledMOFOutput = '{0}\output\MOF' -f $DatumModulePath
+            CompileRequiredModulesOutput = '{0}\output\RequiredModules' -f $DatumModulePath
+            BuildPath = '{0}\build.ps1' -f $DatumModulePath
+        }
+    
+    }
+
 }
+
--- ScriptRunnerScripts/Module/Public/Start-SRDSConfiguration.ps1
+++ ScriptRunnerScripts/Module/Public/Start-SRDSConfiguration.ps1
@@ -1,2 +1,63 @@
 #
 # Reads the node configuration to get the GUID
+
+function Start-SRDSConfiguration {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [Switch]
+        $ResolveDependency
+    )
+    
+    #
+    #  Start the Build Process
+
+    . $Global:ScriptRunner.DatumModule.BuildPath @PSBoundParameters
+
+    #
+    # Once the build process has been completed, load the MOF Node Registration File
+    $MOFNodeRegistrationFile = Import-Clixml -LiteralPath $Global:ScriptRunner.DSCPullServer.MOFNodeRegistrationFile
+
+    # Create RenamedMOFOutput directory in the output directory.
+    
+    $MOFDestinationDir = $(
+        if (Test-Path -LiteralPath $Global:ScriptRunner.DatumModule.RenamedMOFOutput) {
+            Get-Item -LiteralPath $Global:ScriptRunner.DatumModule.RenamedMOFOutput
+        } else {
+            New-Item -Path $Global:ScriptRunner.DatumModule.RenamedMOFOutput -Type Directory -ErrorAction Stop
+        }
+    )   
+    
+    # This is where the compiled mof's will be copied to.
+
+    #
+    # Iterate through each of the mof files in 
+    Get-ChildItem -LiteralPath $Global:ScriptRunner.DatumModule.CompiledMOFOutput -File -Recurse | ForEach-Object {
+        
+        # Get the Item
+        $item = $_
+
+        # Match the NodeName to the FileName.
+        $matchedItem = $MOFNodeRegistrationFile | Where-Object { $_.NodeName -eq $item.Name.Split('.')[0] }
+
+        # Don't copy any MOF files that arn't stored within the configuration file.
+        if ($matchedItem.count -eq 0) { return }
+
+        # Construct the destination file name.
+        # This needs to include the .mof file and .mof.checksum file.
+        $destinationFileName = "{0}\{1}.{2}" -f 
+            $MOFDestinationDir.FullName
+            $matchedItem.registrationId,
+            $item.name.split('.')[1..$item.name.split('.').Length] -join '.'
+
+        # Copy the file
+        $null = Copy-Item $_.FullName -Destination $destinationFileName -Force
+
+    }
+
+    #
+    # Once that's completed, copy MOFFiles and Resource Directories over to the DSCPullServer
+
+    #TODO: Add logic to copy mof configurations to DSC pull server.
+
+}
