05a194fe9ce38c87fc8086b7b6885f028017aa46
Michael Zanatta <mzanatta@outlook.com.au>
2022-07-30 08:51:01 +1000

    Daily Commit Added: Format-YAMLObject Find-YamlValue
    Used to dynamically search
    YAML object structure.

--- ScriptRunnerScripts/Module/Private/Find-YAMLValue.ps1
+++ ScriptRunnerScripts/Module/Private/Find-YAMLValue.ps1
@@ -1,24 +1,84 @@
-<#
+
+
+function ConvertYAMLPathTo-Parameter ([String]$input) {
+
+    # $FormattedYAML."NetworkIpConfiguration"."Interfaces"."DnsServer"[1]
+
+    
+
+}
+
 function Find-YamlValue {
     [CmdletBinding()]
     param (
         [Object]$YAMLObject,
-        [String]$ValueToFind,
-        [String]$Parent
+        [String]$ValueToFind
     )
 
-    process {
-        
+
+    switch ($YAMLObject) {
+
         #
-        # Iterate through each of the keys within the YAML file
-        $type = 
+        # If the YAMLObject is a hashtable, iterate through each of the properties and
+        # prase the object in.
 
-        if ($YAMLObject -is [])
-
-    }
+        {$YAMLObject.GetType().Name -eq 'Hashtable'} {
     
+            ForEach($Key in $YAMLObject.Keys) {
+
+                $params = @{
+                    YAMLObject = $YAMLObject[$Key]
+                    ValueToFind = $ValueToFind
+                }
+
+                Find-YamlValue @params
+
+            }
+
+            break;
+
+        }
+
+        #
+        # If the YAML Object type is an array.
+        # Iterate through each of the items in the array
+
+        {$YAMLObject.GetType().BaseType.Name -eq 'Array'} {
+
+            $newYAMLObject = @()
+            #
+            # Iterate through each of the index items and call them
+            For($index = 0; $index -ne $YAMLObject.Count; $index++) {
+
+                $params = @{
+                    YAMLObject = $YAMLObject[$index]
+                    ValueToFind = $ValueToFind
+                }
+
+                Find-YamlValue @params 
+    
+            }
+
+        }
+
+        #
+        # These are property types. (String, Int, DateTime)
+        
+        default {
+
+            if ($YAMLObject.ToString() -match $ValueToFind) {
+                [PSCustomObject]@{
+                    Value = $YAMLObject
+                    Path = $YAMLObject._YAMLPath
+                }
+            }
+
+        }
+
+    }   
+
 }
-#>
+
 
 
 #
@@ -29,9 +89,25 @@ Function Format-YAMLObject {
     [CmdletBinding()]
     param (
         [Object]$YAMLObject,        
-        [String]$ParentPropertyName,
-        [String]$ParentTypeName,
-        [Int]$ParentIndexValue = -1
+        [String]$YAMLLookupPath = "",
+        [String]$ObjectName
+    )
+
+    #
+    # Format the Parent Path
+
+    $ParentPath = $(
+
+        #
+        # Top-Level Object
+        if ([String]::IsNullOrEmpty($YAMLLookupPath)) {
+            # Object name must exist if $YAMLLookupPath is null
+            if ([String]::IsNullOrEmpty($ObjectName)) { Throw "Error... ObjectName, must be defined!" }
+            '${0}' -f $ObjectName
+        } else {
+            '{0}' -f $YAMLLookupPath
+        }
+
     )
 
     # Create an HashTable, with property information that contains
@@ -46,28 +122,15 @@ Function Format-YAMLObject {
 
         {$YAMLObject.GetType().Name -eq 'Hashtable'} {
 
-            #
-            # Iterate through each of the key's and recursivly call the function
-            $newYAMLObject = @{}
-            $newYAMLObject = $newYAMLObject | Add-Member -MemberType NoteProperty -Name '_YAMLParentInfo' -Value @() -Force
-            $newYAMLObject._YAMLParentInfo += $([PSCustomObject]@{
-                ParentPropertyName = $ParentPropertyName
-                ParentTypeName = $ParentTypeName
-                ParentIndexValue = $ParentIndexValue
-            })
-
+            $newYAMLObject = @{}            
             ForEach($Key in $YAMLObject.Keys) {
 
                 $params = @{
                     YAMLObject = $YAMLObject[$Key]
-                    ParentPropertyName = $Key
-                    ParentTypeName = $YAMLObject.GetType().Name
-                    ParentIndexValue = -1
+                    YAMLLookupPath = '{0}."{1}"' -f $ParentPath, $Key
                 }
 
-                Write-Host $Key
                 $newYAMLObject."$Key" = Format-YAMLObject @params
-                #$YAMLObject[$Key] = Format-YAMLObject @params
 
             }
 
@@ -82,41 +145,46 @@ Function Format-YAMLObject {
         {$YAMLObject.GetType().Name -eq 'List`1'} {
 
             $newYAMLObject = @()
-            #$newYAMLObject = [System.Collections.ArrayList]::new()
+            #$newYAMLObject = [System.Collections.Generic.List[Object]]::new()
 
             #
-            # Iterate through each of the index items and call them
-            For($index = 0; $index -ne $YAMLObject.Count; $index++) {
+            # If the index of the array is zero, then don't add an index item to it.
+            # Otherwise, iterate through the index of the array.
+
+            if ($YAMLObject.Count -eq 1) {
 
                 $params = @{
-                    YAMLObject = $YAMLObject[$index]
-                    ParentPropertyName = $ParentPropertyName
-                    ParentTypeName = $YAMLObject.GetType().Name
-                    ParentIndexValue = $index
+                    YAMLObject = $YAMLObject[0]
+                    YAMLLookupPath = '{0}' -f $ParentPath
                 }
 
-                #$newYAMLObject.Add((Format-YAMLObject @params))
-                
-                $YAMLObject[$index] = Format-YAMLObject @params 
-                
-                $YAMLObject[$index]._YAMLParentInfo += [PSCustomObject]@{
-                    ParentPropertyName = $ParentPropertyName
-                    ParentTypeName = $ParentTypeName
-                    ParentIndexValue = $ParentIndexValue
+                $newYAMLObject += Format-YAMLObject @params
+
+            } else {
+
+                #
+                # Iterate through each of the index items and call them
+                For($index = 0; $index -ne $YAMLObject.Count; $index++) {
+
+                    $params = @{
+                        YAMLObject = $YAMLObject[$index]
+                        YAMLLookupPath = '{0}[{1}]' -f $ParentPath, $index
+                    }
+
+                    $newYAMLObject += Format-YAMLObject @params
+    
                 }
 
             }
-
         }
 
+        #
+        # These are property types. (String, Int, DateTime)
+        
         default {
 
             $newYAMLObject = $YAMLObject
-            $newYAMLObject = $newYAMLObject | Add-Member -MemberType NoteProperty -Name '_YAMLParentInfo' -Value $([PSCustomObject]@{
-                ParentPropertyName = $ParentPropertyName
-                ParentTypeName = $ParentTypeName
-                ParentIndexValue = $ParentIndexValue
-            }) -PassThru -Force 
+            $newYAMLObject = $newYAMLObject | Add-Member -MemberType NoteProperty -Name '_YAMLPath' -Value $ParentPath -Force -PassThru
 
         }
 
