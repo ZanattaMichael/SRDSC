3c0b516113890010da4f3db8e6adef7ccb4c9a29
Michael Zanatta <mzanatta@outlook.com.au>
2022-08-08 06:21:22 +1000

    Starting work on adding DSC Provisioning Script

--- ScriptRunnerScripts/Module/Private/New-DSCPullServer.ps1
+++ ScriptRunnerScripts/Module/Private/New-DSCPullServer.ps1
@@ -10,48 +10,7 @@ Function New-DSCPullServer {
 
     )
 
-    #
-    # Ensure that the DSC Pull Server has the latest version of PowerShellGet
-    Invoke-Command -ComputerName $DSCPullServer -ScriptBlock {
 
-        if ((Get-PackageProvider PowerShellGet).Version.Major -le 1) {
-            #
-            # Using documentation from https://docs.microsoft.com/en-us/powershell/scripting/gallery/installing-psget?view=powershell-5.1
-
-            # Set the TLS Settings
-            [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12   
-            # Update the PowerShell Package Provider, by first updating nuget
-            $null = Install-PackageProvider -Name NuGet -Force
-            # Update PowerShell Get
-            $null = Install-Module PowerShellGet -AllowClobber -Force -Confirm:$false
-            # Set the PowerShellGet Repo to be Trusted
-            $null = Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-        }
-
-    }
-
-    #
-    # Generate Self Signed Certificate on the DSC Pull Server.
-    # If the file already exists, stop.
-    Invoke-Command -ComputerName $DSCPullServer -ScriptBlock {
-
-        # Check if the certificate already exists. If so, stop.
-        if (
-            [Array](Get-ChildItem Cert:\LocalMachine\ -Recurse |
-                        Where-Object {$_.Subject -like ('*DSC.{0}*' -f $ENV:USERDNSDOMAIN)}).Count -ne 0
-        ) { 
-            Write-Verbose "Certificate Already Exists. Using Existing Certificate."
-            return 
-        }
-
-        # Provision a new certificate.
-        # DNSName will be a dsc prefix with the userdns domain as a suffix: .contoso.local
-
-        $params = @{
-            DNSName = "DSC.{0}" -f $ENV:USERDNSDOMAIN 
-            CertStoreLocation = "cert:\LocalMachine\My"
-        }
-        New-SelfSignedCertificate @params
 
     }
 
