736599f52471d071faecf2555da78fee5ce06d20
Michael Zanatta <mzanatta@outlook.com.au>
2022-11-26 15:06:43 +1000

    Add Build Script

--- _Build/BuildModule.ps1
+++ _Build/BuildModule.ps1
@@ -3,45 +3,43 @@
 Write-Verbose "Building PowerShell Module:"
 
 $BuildDirectory = Split-Path -parent $PSCommandPath
-$BuildModuleDirectory = [System.IO.Path]::Join($BuildDirectory, "SelMVP")
-$BuildModuleFile = [System.IO.Path]::Join($BuildModuleDirectory, "Module.psm1")
-$BuildModulePSDFile = [System.IO.Path]::Join($BuildModuleDirectory, "SelMVP.psd1")
-$BuildModuleDirectoryLibraries = [System.IO.Path]::Join($BuildModuleDirectory)
-$BuildModuleDirectoryContributions = [System.IO.Path]::Join($BuildModuleDirectory, "Contributions")
+$BuildModuleDirectory = [System.IO.Path]::Join($BuildDirectory, "SRDSC")
+$BuildModulePSDFile = [System.IO.Path]::Join($BuildModuleDirectory, "SRDSC.psd1")
 
-$ModuleDirectory = $BuildDirectory -replace "Build","Module"
-$ModuleFile = [System.IO.Path]::Join($ModuleDirectory, "Module.psm1")
-$ModuleDirectoryPrivateFunctions = [System.IO.Path]::Join($ModuleDirectory, "Functions","Private")
-$ModuleDirectoryPublicFunctions = [System.IO.Path]::Join($ModuleDirectory, "Functions","Public","Cmdlets")
-$ModuleDirectoryDLLibraries = [System.IO.Path]::Join($ModuleDirectory, "Libraries")
-$ModuleDirectoryContributions = [System.IO.Path]::Join($ModuleDirectory, "Functions","Public","Contributions")
+$ModuleDirectory = [System.IO.Path]::Join((Split-Path $BuildDirectory -Parent),'Module')
 
-$ModuleResourceData = [System.IO.Path]::Join($ModuleDirectory, "Resources")
-
-$ModuleManifestParams = @{
-    RootModule = 'Module.psm1'
-    Path = $BuildModulePSDFile
-    Guid = [GUID]::NewGuid().Guid
-    Author = 'Michael.Zanatta'
-    ModuleVersion = Get-Content -Path (Join-Path -Path $BuildDirectory -ChildPath '.\BuildVersion.txt' ) | Select-Object -Last 1
-    Description = ""
-    PowerShellVersion = '5.1'
-    RequiredModules = ''
-    NestedModules = ''
-    FunctionsToExport = @()
-    RequiredAssemblies = "Libraries\HTMLAgilityPack\HtmlAgilityPack.dll"
+$params = @{
+    LiteralPath = $ModuleDirectory
+    Destination = $BuildModuleDirectory
+    Force = $true
+    Recurse = $true
 }
 
-Write-Debug "`$BuildDirectory: $BuildDirectory"
-Write-Debug "`$BuildModuleDirectory: $BuildModuleDirectory"
-Write-Debug "`$BuildModuleDirectoryLibraries: $BuildModuleDirectoryLibraries"
-Write-Debug "`$BuildModuleDirectoryContributions: $BuildModuleDirectoryContributions"
-Write-Debug "`$ModuleFile: $ModuleFile"
-Write-Debug "`$ModuleDirectoryPrivateFunctions: $ModuleDirectoryPrivateFunctions"
-Write-Debug "`$ModuleDirectoryPublicFunctions: $ModuleDirectoryPublicFunctions"
-Write-Debug "`$ModuleDirectoryDLLibraries: $ModuleDirectoryDLLibraries"
-Write-Debug "`$ModuleDirectoryContributions: $ModuleDirectoryContributions"
-Write-Debug "`$ModuleDirectoryLocalizedData: $ModuleDirectoryLocalizedData"
+Copy-Item @params
+
+$ModuleManifestParams = @{
+    RootModule = 'SRDSC.psm1'
+    Path = $BuildModulePSDFile
+    Guid = [GUID]::NewGuid().Guid
+    Author = 'Michael Zanatta'
+    ModuleVersion = Get-Content -Path (Join-Path -Path $BuildDirectory -ChildPath '.\BuildVersion.txt' ) | Select-Object -Last 1
+    Description = "Script Runner meet Desired State Configuration! Desired State Configuration meet Script Runner! SRDSC is a PowerShell Module that integrates Script Runner's portal with Desired State Configuration (using the DSC Toolbox with Datum). It's intention is to enable non-PowerShell users to create infrastructure services, abstracting away the complexities of understanding CaC (Configuration as Code) while Infrastructure teams retain control over key services."
+    PowerShellVersion = '5.1'
+    RequiredModules = 'powershell-yaml'
+    FunctionsToExport = @()
+}
+
+#
+# Locate all functions that need to be exported.
+
+Get-ChildItem 'D:\Git\DSC-ScriptRunner\DSC-ScriptRunner\Module\Public' -Recurse -File | Select-Object {
+    $ModuleManifestParams.FunctionsToExport += $_.BaseName
+}
+Get-ChildItem 'D:\Git\DSC-ScriptRunner\DSC-ScriptRunner\Module\Private' -Recurse -File | ForEach-Object {
+    if ((Get-Content $_.FullName) -match 'Export-ModuleMember') {
+        $ModuleManifestParams.FunctionsToExport += $_.BaseName
+    }
+}
 
 #
 # Create Module Manifest
